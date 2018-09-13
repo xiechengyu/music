037218f0ca0f48b086027033e0e3bc9b749a613f
xiechengyu <2390936921@qq.com>
2018-09-13 20:18:34 +0800

    Initial commit

---
+++ src/store/actions.js
@@ -0,0 +1,17 @@
+import * as types from './mutation-types'
+
+/*export const selectPlay = ({commit,state},{list,index}) => {
+  commit(types.SET_SEQRUENCE_LIST,list)
+  commit(types.SET_PLAYLIST,list)
+  commit(types.SET_CURRENT_INDEX,index)
+  commit(types.SET_FULL_SCREEN,true)
+  commit(types.SET_PLAYING_STATE,true)
+}*/
+
+export const selectPlay = function ({commit,state},{list,index}) {
+  commit(types.SET_SEQRUENCE_LIST,list)
+  commit(types.SET_PLAYLIST,list)
+  commit(types.SET_CURRENT_INDEX,index)
+  commit(types.SET_FULL_SCREEN,true)
+  commit(types.SET_PLAYING_STATE,true)
+}
